e558a45b5915142b4d647e91ed7680b4ffa17d3b
kishore tvk <kishoretvk@hotmail.com>
2017-09-10 16:05:39 -0500

    commit to git

---
+++ LlinkedList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LlinkedList
+{
+
+     class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new Node { Value = 11 };
+            var middle = new Node { Value = 12 };
+            var last = new Node { Value = 14 };
+           
+
+            //making linked List here !
+            first.Next = middle;
+            middle.Next = last;
+            Print(first);
+            Console.WriteLine("Hello World!");
+            Console.Read();
+
+        }
+        private static void Print(Node node)
+        {
+            Console.WriteLine("printing List!");
+            while (node != null)
+            {
+
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+            Console.WriteLine("printing list completed!");
+        }
+    }
+}
